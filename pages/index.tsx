994bf0c7c2007befc5b0ea412eeb70d9889aba2f
marcelc63 <marcelc6363@gmail.com>
2021-09-15 19:48:29 +0700

    Generate tailwind classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -319,6 +319,15 @@ export default function Home() {
           </div>
         )}
       </Modal>
+      <Modal open={false}>
+        <p className="text-md font-light animate-pulse">Test</p>
+        <p className="text-lg font-normal animate-pulse">Test</p>
+        <p className="text-xl font-medium">Test</p>
+        <p className="text-2xl font-semibold">Test</p>
+        <p className="text-3xl font-bold">Test</p>
+        <p className="text-4xl font-extrabold">Test</p>
+        <p className="text-4xl font-black">Test</p>
+      </Modal>
     </div>
   )
 }
